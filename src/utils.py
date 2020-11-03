8c5983a3e71bbcc43c3f9ad6d73bbe7d4f646f8b
danielsto <danielsto@protonmail.com>
2020-11-03 19:05:43 +0000

    Added docstrings to utils.py functions

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,9 @@ with open("./data/teams.json") as json_file:
 
 
 def get_end_year(season):
+    """
+    Returns the end year given a season in YYYY-YY format
+    """
     second_part = season.split('-')[1]
     first_part = '20' if second_part == '00' else season.split('-')[0][:2]
     year = f'{first_part}{second_part}'
@@ -19,6 +22,10 @@ def get_end_year(season):
 
 
 def treat_input(leader, type="integer"):
+    """
+    Returns the score given an input that has mixed name (string) and score
+    (integer/float) like L. James (987).
+    """
     if re.findall(REGEX_NUMS, leader):
         if type == "integer":
             score = int(re.findall(REGEX_NUMS, leader)[0])
@@ -30,15 +37,19 @@ def treat_input(leader, type="integer"):
 
 
 def get_player_data(player, row, season, mode):
+    """
+    Scrapes player profile to return their contry, team and age
+    """
     player_rel_url = row.find("td", {"data-stat": player}).find("a")
     if not player_rel_url or mode == 'simple':
         return None, None, None
     else:
         player_url = BASE_URL + player_rel_url['href']
+
     req = requests.get(player_url)
     status_code = req.status_code
+
     if status_code == 200:
-        # print('[Player] \033[92m200 OK\033[0m')
         soup = bs4.BeautifulSoup(req.text, "html.parser")
 
         if soup.find("span", {"class": "f-i f-us"}):
